edf7e3e3b1b3a4edcadd6b34f7da2a8c534444b9
Prateek <Prateek>
2022-01-07 11:43:27 +0530

    Middleware calling order in automation routes

--- routes/automation.js
+++ routes/automation.js
@@ -1,9 +1,12 @@
 // const validationMiddleware = require('../middlewares/validationMiddleware');
 const c = require('../utils/handlers/controller');
+
+//Middlewares
 const checkTokenAuthentication = require('../auth/UserAuthentication');
 const authorize = require('../auth/UserAuthorization');
 const validationMiddleware = require('../middlewares/requestValidation');
 const stringifiedObjectParser = require('../middlewares/stringifiedObjectParser');
+const groupEntityValidation = require('../middlewares/groupEntityValidation');
 
 //Controllers
 const automationController = require('../controllers/automation');
@@ -17,18 +20,20 @@ const Roles = require('../utils/helpers/roles');
 
 module.exports = (router, app) => {
     router.post('/changeAutomationActivationStatus',
-    checkTokenAuthentication, 
+    validationMiddleware,
+    checkTokenAuthentication,
+    groupEntityValidation,
     authorize([Roles.Owner, Roles.Member]),
-    validationMiddleware, 
     c(automationController.changeAutomationActivationStatus, (req, res, next) => [req.body.automationId, req.body.activationStatus,
         res.locals.userId
     ]));
 
     router.post('/createTimeBasedAutomation',
-    checkTokenAuthentication,
-    authorize([Roles.Owner, Roles.Member]),
     stringifiedObjectParser,
     validationMiddleware,
+    checkTokenAuthentication,
+    groupEntityValidation,
+    authorize([Roles.Owner, Roles.Member]),
     c(automationController.createTimeBasedAutomation,(req, res, next) => [req.body.groupId, req.body.automationName, 
         req.body.triggerTime, req.body.triggerWeekDays, req.body.allUsersAccess, req.body.deviceId, req.body.actions, 
         req.body.sceneId, req.body.triggerDate, res.locals.userId
@@ -36,20 +41,22 @@ module.exports = (router, app) => {
 
     //Note - might need to add automationAuth so that only group owner and automation creator can update the automation
     router.post('/updateTimeBasedAutomation',
-    checkTokenAuthentication,
-    authorize([Roles.Owner, Roles.Member]),
     stringifiedObjectParser,
     validationMiddleware,
+    checkTokenAuthentication,
+    groupEntityValidation,
+    authorize([Roles.Owner, Roles.Member]),
     c(automationController.updateTimeBasedAutomation, (req, res, next) => [req.body.groupId, req.body.automationId,
         req.body.automationName,req.body.triggerTime, req.body.triggerWeekDays, req.body.deviceId, req.body.actions, 
         req.body.sceneId, req.body.triggerDate, res.locals.userId
     ]));
 
     router.post('/createConditionBasedAutomation',
-    checkTokenAuthentication,
-    authorize([Roles.Owner, Roles.Member]),
     stringifiedObjectParser,
     validationMiddleware,
+    checkTokenAuthentication,
+    groupEntityValidation,
+    authorize([Roles.Owner, Roles.Member]),
     c(automationController.createConditionBasedAutomation, (req, res, next) => [req.body.groupId, req.body.automationName,
         req.body.triggerDeviceId, req.body.triggerDeviceDataType, req.body.triggerDeviceValue, req.body.triggerDeviceCondition, 
         req.body.allUsersAccess, req.body.deviceId, req.body.actions, req.body.sceneId, req.body.notify, res.locals.userId
@@ -57,10 +64,11 @@ module.exports = (router, app) => {
 
     //Note - might need to add automationAuth so that only group owner and automation creator can update the automation
     router.post('/updateConditionBasedAutomation',
-    checkTokenAuthentication,
-    authorize([Roles.Owner, Roles.Member]),
     stringifiedObjectParser,
     validationMiddleware,
+    checkTokenAuthentication,
+    groupEntityValidation,
+    authorize([Roles.Owner, Roles.Member]),
     c(automationController.updateConditionBasedAutomation, (req, res, next) => [req.body.groupId, req.body.automationId,
         req.body.automationName, req.body.triggerDeviceId, req.body.triggerDeviceDataType, req.body.triggerDeviceValue, 
         req.body.triggerDeviceCondition, req.body.allUsersAccess, req.body.deviceId, req.body.actions, req.body.sceneId,
@@ -68,31 +76,35 @@ module.exports = (router, app) => {
     ]));
 
     router.post('/deleteAutomation',
-    checkTokenAuthentication,
-    authorize([Roles.Owner]),
     validationMiddleware,
+    checkTokenAuthentication,
+    groupEntityValidation,
+    authorize([Roles.Owner]),
     c(automationController.deleteAutomation, (req, res, next) => [req.body.groupId, req.body.automationId, 
         res.locals.userId
     ]));
 
     router.post('/fetchAutomationData',
-    checkTokenAuthentication,
-    authorize([Roles.Owner, Roles.Member]),
     validationMiddleware,
+    checkTokenAuthentication,
+    groupEntityValidation,
+    authorize([Roles.Owner, Roles.Member]),
     c(automationController.fetchAutomationData, (req, res, next) => [req.body.automationId, res.locals.userId]));
 
     router.post('/changeAutomationApprovalStatus',
-    checkTokenAuthentication,
-    authorize([Roles.Owner]),
     validationMiddleware,
+    checkTokenAuthentication,
+    groupEntityValidation,
+    authorize([Roles.Owner]),
     c(automationController.changeAutomationApprovalStatus, (req, res, next) => [req.body.groupId, req.body.automationId, 
         req.body.approvalStatus, res.locals.userId
     ]));
 
     router.post('/fetchAutomationByDevicesAndScenes',
-    checkTokenAuthentication,
-    authorize([Roles.Owner, Roles.Member]),
     validationMiddleware,
+    checkTokenAuthentication,
+    groupEntityValidation,
+    authorize([Roles.Owner, Roles.Member]),
     c(automationController.fetchAutomationByDevicesAndScenes, (req, res, next) => [req.body.groupId, req.body.deviceIds, 
         req.body.sceneIds, res.locals.userId
     ]));
